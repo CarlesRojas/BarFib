dd32290c26f9ce8c7796fdf946855e3c98b7871a
Jaume Ballester <jballesterpuig@gmail.com>
2018-10-21 01:04:15 +0200

    fix

--- src/Screens/BuyScreen.js
+++ src/Screens/BuyScreen.js
@@ -10,46 +10,42 @@ import {
 import Header from "../components/Header";
 import InfoPanel from "../components/InfoPanel";
 import BuyButton from "../components/BuyButton";
-
 import Subscription from "../components/Subscription";
-
 import {connect} from 'react-redux'
 import {fetchProduct} from "../actions/buyActions";
 
-
 class BuyScreen extends React.Component {
-    componentWillMount() {
+    componentWillMount(){
         this.props.fetchProduct(this.props.id)
 
     }
-
     navigate = (destiny) => {
         this.props.navigation.navigate(destiny);
     }
+    select = () =>{
 
+    }
 
     render() {
         return (
             <ImageBackground
-                source={require('../Img/coffee_bg.png')}
+                source = {require('../Img/coffee_bg.png')}
                 style={styles.container}>
 
                 <Header topImg={require('../Img/coffee.png')}/>
-                <View style={styles.menuContainer}>
-                    <InfoPanel amplada={'85%'} onPress={() => {
-                        this.navigate('Home')
-                    }} ar={1210 / 400} name={this.props.product ? this.props.product.name : ''}
+
+                <View style={styles.infoContainer}>
+                    <InfoPanel amplada={'85%'} onPress={() => {this.navigate('Home')}} ar={1210/400}  name={this.props.product ? this.props.product.name : ''}
                                price={this.props.product ? this.props.product.price : ''}/>
                 </View>
 
                 <View style={styles.buyContainer}>
-                    <BuyButton amplada={'50%'} ar={1210 / 400}/>
+                    <BuyButton amplada={'50%'} ar={1210/400}/>
                 </View>
 
-
                 <View style={styles.subsContainer}>
-                    <Subscription amplada={'90%'} ar={1210 / 220} infoSub={'1 coffee/day   25€/month'}/>
-                    <Subscription amplada={'90%'} ar={1210 / 220} infoSub={'5 coffees/week   5€/week'}/>
+                    <Subscription amplada={'90%'} ar={1210/220} infoSub={'1 coffee/day   25€/month'}/>
+                    <Subscription amplada={'90%'} ar={1210/220} infoSub={'5 coffees/week   5€/week'}/>
                 </View>
 
             </ImageBackground>
@@ -58,14 +54,12 @@ class BuyScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
-        flex: 1,
-        width: '100%',
+    container:{
+        width:  '100%',
         height: '100%',
     },
     infoContainer: {
-        alignItems: 'center',
+        alignItems:'center',
         elevation: 10,
         position: 'absolute',
         top: '25%',
@@ -73,9 +67,9 @@ const styles = StyleSheet.create({
         height: '30%',
     },
     buyContainer: {
-        marginTop: '25%',
-        marginBottom: '5%',
-        alignItems: 'center',
+        marginTop:'25%',
+        marginBottom:'5%',
+        alignItems:'center',
         width: '100%',
     },
     subsContainer: {
@@ -84,10 +78,9 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '30%',
         alignItems: 'center',
-        justifyContent: 'flex-end'
+        justifyContent:'flex-end'
     }
 });
-
 const mapStateToProps = (state, ownProps) => {
     console.log(ownProps)
     return {
@@ -96,9 +89,9 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
+const  mapDispatchToProps = (dispatch)=>{
     return {
-        fetchProduct: (id) => dispatch(fetchProduct(id))
+        fetchProduct: (id)=>dispatch(fetchProduct(id))
     }
 }
 
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
         flex: 1,
         width:  '100%',
         height: '100%',
-        backgroundColor: 'transpparent',
+        backgroundColor: 'transparent',
     },
     menuContainer: {
         width: '100%',
